69289c0476147e75d24b2e5e425af60c74790534
goenawork <goenawork@gmail.com>
2026-06-13 09:17:56 +0700

    script.js

--- script.js
+++ script.js
@@ -20,3 +20,49 @@ button.addEventListener("click", async () => {
     }
 
 });
+const video = document.getElementById("video");
+const startCamera = document.getElementById("startCamera");
+
+const takePhoto = document.getElementById("takePhoto");
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const photo = document.getElementById("photo");
+
+startCamera.addEventListener("click", async () => {
+
+    try {
+
+        const stream =
+            await navigator.mediaDevices.getUserMedia({
+                video: true
+            });
+
+        video.srcObject = stream;
+
+    } catch(err) {
+
+        alert("Kamera tidak dapat diakses");
+        console.error(err);
+
+    }
+
+});
+
+takePhoto.addEventListener("click", () => {
+
+    ctx.drawImage(
+        video,
+        0,
+        0,
+        canvas.width,
+        canvas.height
+    );
+
+    const imageData =
+        canvas.toDataURL("image/png");
+
+    photo.src = imageData;
+
+});
